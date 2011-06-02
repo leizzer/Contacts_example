b1c86633eeacb61cc2139afb9a776aeb15adc188
Cristian Re <cristian@szstudios.net>
2011-06-02 11:26:14 -0300

    starting with kulesa-contacts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = '967708b3ba1b4f83b36a577e67cfdd9d29973146e3deff79aefb04dce04a398b1df829c85638685a06ca875b82e4c816e1a1fc3e6e7d939ba781d46a71e62448'
